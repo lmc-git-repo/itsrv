c0cc0bb1804a7d1f57498d88bb0a6fb4db5e74a8
liezel <larracasliezel31@gmail.com>
2026-02-25 08:52:22 +0800

    Fix pagination tables of Tickets, Transfer Slip & Trouble Report

--- resources/js/Pages/Tickets/Index.jsx
+++ resources/js/Pages/Tickets/Index.jsx
@@ -284,6 +284,24 @@ export default function TicketsIndex({ tickets = [] }) {
                     </div>
                 </div>
             )}
+
+            {links.length > 0 && (
+                <div className="flex justify-center mt-6 gap-2 text-sm">
+                    {links.map((link, i) => (
+                        <button
+                            key={i}
+                            disabled={!link.url}
+                            onClick={() => link.url && router.visit(link.url)}
+                            className={`px-3 py-1 rounded ${
+                                link.active
+                                    ? "bg-blue-600 text-white"
+                                    : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+                            }`}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                        />
+                    ))}
+                </div>
+            )}
         </AuthenticatedLayout>
     );
 }
